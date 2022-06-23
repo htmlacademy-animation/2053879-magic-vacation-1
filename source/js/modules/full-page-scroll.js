2e97f8ea261c57fe883274f994c6ed44a8f1386f
d.mishin <d.mishin@rambler-co.ru>
2022-06-23 09:08:10 +0300

    fix: showing prizes page animation only if pref hash was story

--- source/js/modules/full-page-scroll.js
+++ source/js/modules/full-page-scroll.js
@@ -8,21 +8,24 @@ export default class FullPageScroll {
     this.animationTimeout = null;
 
     this.screenElements = document.querySelectorAll(
-        `.screen:not(.screen--result)`
+      `.screen:not(.screen--result)`
     );
     this.menuElements = document.querySelectorAll(
-        `.page-header__menu .js-menu-link`
+      `.page-header__menu .js-menu-link`
     );
 
     this.activeScreen = 0;
     this.onScrollHandler = this.onScroll.bind(this);
     this.onUrlHashChengedHandler = this.onUrlHashChanged.bind(this);
+
+    // Будем отслеживать историю смены хешей для анимации
+    this.hashHistory = [];
   }
 
   init() {
     document.addEventListener(
-        `wheel`,
-        throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, {trailing: true})
+      `wheel`,
+      throttle(this.onScrollHandler, this.THROTTLE_TIMEOUT, { trailing: true })
     );
     window.addEventListener(`popstate`, this.onUrlHashChengedHandler);
 
@@ -49,13 +52,23 @@ export default class FullPageScroll {
 
   onUrlHashChanged() {
     const newIndex = Array.from(this.screenElements).findIndex(
-        (screen) => location.hash.slice(1) === screen.id
+      (screen) => location.hash.slice(1) === screen.id
     );
 
     if (newIndex === this.activeScreen) {
       return;
     }
 
+    const prevElementClassList =
+      this.screenElements[this.activeScreen].classList;
+
+    // Если у предыдущего элемента остался класс animation-in-progress,
+    // значит анимация не успела отработать, поэтому уберем его
+    if (prevElementClassList.contains(`animation-in-progress`)) {
+      prevElementClassList.remove(`animation-in-progress`);
+    }
+
+    this.hashHistory.push(this.screenElements[this.activeScreen].id);
     this.activeScreen = newIndex < 0 ? 0 : newIndex;
 
     this.changePageDisplay();
@@ -72,9 +85,16 @@ export default class FullPageScroll {
       window.clearTimeout(this.animationTimeout);
     }
 
-    if (this.screenElements[this.activeScreen].id === `prizes`) {
+    const prevHash = this.hashHistory[this.hashHistory.length - 1];
+
+    if (
+      this.screenElements[this.activeScreen].id === `prizes` &&
+      prevHash === `story`
+    ) {
       // Добавим сразу класс active, чтобы запустить анимацию
-      this.screenElements[this.activeScreen].classList.add(`active`);
+      this.screenElements[this.activeScreen].classList.add(
+        `animation-in-progress`
+      );
 
       // Запустим таймер, чтобы сначала отработала анимация для показывания блока,
       // а затем уже поменяем контент страницы
@@ -84,7 +104,13 @@ export default class FullPageScroll {
           screen.classList.remove(`active`);
         });
 
-        this.screenElements[this.activeScreen].classList.remove(`screen--hidden`);
+        this.screenElements[this.activeScreen].classList.remove(
+          `screen--hidden`
+        );
+
+        this.screenElements[this.activeScreen].classList.remove(
+          `animation-in-progress`
+        );
       }, 1000);
 
       return;
@@ -106,7 +132,7 @@ export default class FullPageScroll {
 
   changeActiveMenuItem() {
     const activeItem = Array.from(this.menuElements).find(
-        (item) => item.dataset.href === this.screenElements[this.activeScreen].id
+      (item) => item.dataset.href === this.screenElements[this.activeScreen].id
     );
     if (activeItem) {
       this.menuElements.forEach((item) => item.classList.remove(`active`));
@@ -129,8 +155,8 @@ export default class FullPageScroll {
   reCalculateActiveScreenPosition(delta) {
     if (delta > 0) {
       this.activeScreen = Math.min(
-          this.screenElements.length - 1,
-          ++this.activeScreen
+        this.screenElements.length - 1,
+        ++this.activeScreen
       );
     } else {
       this.activeScreen = Math.max(0, --this.activeScreen);
