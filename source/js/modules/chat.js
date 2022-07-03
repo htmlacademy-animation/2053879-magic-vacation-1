46c62d7fd46cd76b769229aa6f1ead1113339792
Dmitry <dmishin.dev@gmail.com>
2022-07-03 17:03:57 +0300

    feat: made module 2 task 3

--- source/js/modules/chat.js
+++ source/js/modules/chat.js
@@ -18,15 +18,19 @@ export default () => {
         let answerEl = document.createElement(`li`);
         let placeholder = document.createElement(`div`);
         let textEl = document.createElement(`p`);
+
         placeholder.classList.add(`chat__placeholder`);
+
         for (let i = 0; i < 3; i++) {
           let dot = document.createElement(`span`);
           placeholder.appendChild(dot);
         }
+
         answerEl.appendChild(placeholder);
         answerEl.classList.add(`chat__message`);
         answerEl.classList.add(`chat__message--incoming`);
         answerEl.classList.add(`chat__message--last`);
+
         let answer = Math.floor(Math.random() * 2);
         let answerText;
 
@@ -40,17 +44,24 @@ export default () => {
         textEl.classList.add(`hidden`);
         answerEl.appendChild(textEl);
         messageList.appendChild(answerEl);
+
         scrollToBottom();
 
         setTimeout(function () {
           let lastMessage = document.querySelector(`.chat__message--last`);
+
           if (lastMessage) {
-            let lastMessagePlaceholder = lastMessage.querySelector(`.chat__placeholder`);
+            let lastMessagePlaceholder =
+              lastMessage.querySelector(`.chat__placeholder`);
+
             let lastMessageText = lastMessage.querySelector(`p`);
+
             lastMessagePlaceholder.classList.add(`chat__placeholder--hidden`);
+
             setTimeout(function () {
               lastMessagePlaceholder.remove();
             }, 400);
+
             lastMessageText.classList.remove(`hidden`);
             lastMessage.classList.remove(`chat__message--last`);
           }
@@ -61,17 +72,22 @@ export default () => {
     let postQuestion = function () {
       if (messageField.value) {
         let messageEl = document.createElement(`li`);
+
         messageEl.classList.add(`chat__message`);
+
         let messageText = messageField.value;
         let text = document.createElement(`p`);
+
         text.innerText = messageText;
+
         messageEl.appendChild(text);
         messageEl.classList.add(`chat__message--outcoming`);
+
         messageList.appendChild(messageEl);
         messageField.value = ``;
         messageField.setAttribute(`disabled`, `true`);
-        scrollToBottom();
 
+        scrollToBottom();
         getAnswer();
 
         messageField.removeAttribute(`disabled`);
@@ -80,6 +96,5 @@ export default () => {
     };
 
     postQuestion();
-
   });
 };
--- source/js/modules/footer.js
+++ source/js/modules/footer.js
@@ -6,7 +6,16 @@ export default () => {
       footerTogglers[i].addEventListener(`click`, function () {
         let footer = footerTogglers[i].parentNode;
         if (footer.classList.contains(`screen__footer--full`)) {
-          footer.classList.remove(`screen__footer--full`);
+          footer.classList.add(`hiding`);
+
+          const animationEnd = () => {
+            footer.removeEventListener(`animationend`, animationEnd);
+
+            footer.classList.remove(`screen__footer--full`);
+            footer.classList.remove(`hiding`);
+          };
+
+          footer.addEventListener(`animationend`, animationEnd);
         } else {
           footer.classList.add(`screen__footer--full`);
         }
